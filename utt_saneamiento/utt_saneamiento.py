6dfac3915fb090eecdaeb26767cf5d9720d8e0e1
miguegomez27-dev <miguegomez27@gmail.com>
2026-04-27 11:55:10 -0300

    Update utt_saneamiento.py

--- utt_saneamiento/utt_saneamiento.py
+++ utt_saneamiento/utt_saneamiento.py
@@ -34,6 +34,9 @@ class UttSaneamientoPlugin:
         self.dialog = None
         self.filter_manager = LayerFilterManager(iface)
         self.ambito_actions = []
+        self.informes_dialog = None
+        self.produccion_dialog = None
+        self.historial_dialog = None
 
     def initGui(self):
         """Registra menu y acciones al cargar el plugin en QGIS."""
@@ -46,7 +49,17 @@ class UttSaneamientoPlugin:
 
         for ambito in self.AMBITOS:
             action = QAction(ambito, self.iface.mainWindow())
-            action.setEnabled(False)
+            if ambito == "Informes":
+                action.setEnabled(True)
+                action.triggered.connect(self.open_informes_dialog)
+            elif ambito == "Produccion":
+                action.setEnabled(True)
+                action.triggered.connect(self.open_produccion_dialog)
+            elif ambito == "Historial de registro grafico":
+                action.setEnabled(True)
+                action.triggered.connect(self.open_historial_dialog)
+            else:
+                action.setEnabled(False)
             self.menu.addAction(action)
             self.ambito_actions.append(action)
 
@@ -60,6 +73,9 @@ class UttSaneamientoPlugin:
             self.menu = None
         self.main_action = None
         self.dialog = None
+        self.informes_dialog = None
+        self.produccion_dialog = None
+        self.historial_dialog = None
         self.ambito_actions = []
 
     def open_dialog(self):
@@ -70,3 +86,37 @@ class UttSaneamientoPlugin:
         self.dialog.show()
         self.dialog.raise_()
         self.dialog.activateWindow()
+
+    def open_informes_dialog(self):
+        """Abre el dialogo del módulo Informes."""
+        if self.informes_dialog is None:
+            from .informes.informes_dialog import UTTInformesDialog
+
+            self.informes_dialog = UTTInformesDialog(self.iface.mainWindow())
+        self.informes_dialog.show()
+        self.informes_dialog.raise_()
+        self.informes_dialog.activateWindow()
+
+
+    def open_produccion_dialog(self):
+        """Abre el dialogo del módulo Producción."""
+        if self.produccion_dialog is None:
+            from .produccion.produccion_dialog import UTTCargaProduccionDialog
+
+            self.produccion_dialog = UTTCargaProduccionDialog(self.iface.mainWindow())
+        self.produccion_dialog.show()
+        self.produccion_dialog.raise_()
+        self.produccion_dialog.activateWindow()
+
+
+    def open_historial_dialog(self):
+        """Abre el dialogo del módulo Historial de registro gráfico."""
+        if self.historial_dialog is None:
+            from .historial.historial_dialog import HistorialRegistroGraficoDialog
+
+            self.historial_dialog = HistorialRegistroGraficoDialog(
+                self.iface, self.iface.mainWindow()
+            )
+        self.historial_dialog.show()
+        self.historial_dialog.raise_()
+        self.historial_dialog.activateWindow()
